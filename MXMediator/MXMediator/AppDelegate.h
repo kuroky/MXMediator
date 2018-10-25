ce8100d9b5c1f6bece84a88c7205d5fa1507e6ef
kurokyfan <kuro2007cumt@gmail.com>
2018-10-25 10:34:19 +0800

    init project

---
+++ MXMediator/MXMediator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXMediator
+//
+//  Created by kuroky on 2018/10/25.
+//  Copyright © 2018 Kuroky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MXMediator/MXMediator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MXMediator
+//
+//  Created by kuroky on 2018/10/25.
+//  Copyright © 2018 Kuroky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
